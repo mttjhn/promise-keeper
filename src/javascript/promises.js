3fab12a34d38fcc208ad6d4882bfa01b8eae2d40
Matthew Johnson <matthew@mcjohnsons.com>
2014-08-16 15:19:25 -0600

    Ran into access-control issues with my use of ESRI's geometry service... so stopping my research for now. But this has been fun!

--- src/javascript/promises.js
+++ src/javascript/promises.js
@@ -1,5 +1,5 @@
 var geocodingUrl = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?f=json&text=';
-var geometryUrl = 'http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer/distance?f=json&distanceUnit=9035';
+var distanceUrl = 'http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer/distance?f=json&distanceUnit=9035';
 
 var getLocation = function(address) {
     return new Promise(function (resolve, reject) {
@@ -26,19 +26,66 @@ var getLocation = function(address) {
     });
 };
 
+var geometryToJSON = function (geometry, geometryType, spatialReference) {
+  var returnGeometry = {
+      "geometryType": geometryType,
+      "gemoetry": {
+          "x": geometry.x,
+          "y": geometry.y,
+          "spatialReference": spatialReference
+      }
+  };
+  return JSON.stringify(returnGeometry);
+};
+
+var getDistance = function(feature1, feature2, spatialReference) {
+    return new Promise(function (resolve, reject) {
+        //Create HTTP Request
+        var request = new XMLHttpRequest();
+        var feature1JSON = geometryToJSON(feature1, 'esriGeometryPoint', spatialReference);
+        var feature2JSON = geometryToJSON(feature2, 'esriGeometryPoint', spatialReference);
+        var requestUrl = distanceUrl + '&geometry1=' + encodeURI(feature1JSON) + '&geometry2=' + encodeURI(feature2JSON) + '&sr=' + encodeURI(spatialReference.wkid);
+
+        request.open('GET', requestUrl);
+
+        request.onload = function () {
+            if (request.status === 200) {
+                resolve(request.response);
+            }
+            else {
+                reject(Error(request.statusText));
+            }
+        };
+
+        request.onerror = function () {
+            reject(Error("Network Error!"));
+        };
+
+        request.send();
+    });
+};
+
 var submitButtonClick = function() {
     var startElement = document.getElementById('startAddress');
     var endElement = document.getElementById('endAddress');
 
     var firstLocation = null;
     var secondLocation = null;
+    var distance = null;
 
     getLocation(startElement.value).then(function (response) {
+        console.log('Found first location!');
         firstLocation = JSON.parse(response);
         getLocation(endElement.value).then(function (response) {
+            console.log('Found second location!');
             secondLocation = JSON.parse(response);
-            console.log('First location: ', firstLocation);
-            console.log('Second location: ', secondLocation);
+            // Now get distance!
+            getDistance(firstLocation.locations[0].feature.geometry, secondLocation.locations[0].feature.geometry, firstLocation.spatialReference).then(function (response) {
+                console.log('Got distance!');
+                distance = JSON.parse(response);
+            }, function (error) {
+                console.log(error);
+            });
         }, function (error) {
             console.log(error);
         });
